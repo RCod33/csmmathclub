1acc06ad6cbfd8a0f0e0cc4ff107335606fa9e7e
Edgar Gonzalez <edgargonzalezgerman@gmail.com>
2025-03-12 12:40:10 -0700

    minor fixings

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
    <div className={styles.homeContainer}>
          <section className={styles.welcomeSection}>
             <h1>Welcome to our club website!</h1>
-            <h4>You can find us on Wednesdays at 18-305 from 1:00pm-2:30pm</h4>
+            <h4>You can find us on Wednesdays at 18-307 from 1:00pm-2:30pm</h4>
             <p>
                The site is still under development, but a fair amount of problems are
                available for you to solve. We hope you enjoy your time here!
