aaad9f6489316ce5b16ff905956e471798a373f7
Ronald <ronaldvinuela@gmail.com>
2025-05-05 16:39:31 +0100

    fix: correct import statement for HashRouter in App.jsx

--- src/Componetns/App/App.jsx
+++ src/Componetns/App/App.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
-import { HashRouterRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import NavBar from "../../Componetns/NavBar/NavBar";
 import Home from "../../pages/Home/Home";
 import ProblemSet from "../../pages/ProblemSet/ProblemSet";
